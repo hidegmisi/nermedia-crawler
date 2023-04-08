2b7191abd7cc0c243ca8dbf02c1f1c9d86374815
Mihály Hideg <hidegmisi@gmail.com>
2023-04-08 07:13:20 +0200

    increase concurrency limit

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ config = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36"
     },
     "output_file": "output.ndjson",
-    "concurrency_limit": 10,
+    "concurrency_limit": 20,
     "save_period": 30
 }
